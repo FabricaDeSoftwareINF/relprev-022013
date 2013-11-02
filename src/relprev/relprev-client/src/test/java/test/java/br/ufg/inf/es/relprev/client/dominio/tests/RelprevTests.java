27c20f0dd6e6de08792f077e966de9769a0a7181
halissonvit <halissonvit@gmail.com>
2013-11-02 05:33:36 +0000

    ref #141 Criando versão inicial(mockada) do client, para que possa ser utilizada no front-end. Com ela já é possível utilizar a api para listar todos os Relprevs, instanciar um novo Relprev, salvar o relprev(novo ou editando)

---
+++ src/relprev/relprev-client/src/test/java/test/java/br/ufg/inf/es/relprev/client/dominio/tests/RelprevTests.java
@@ -0,0 +1,77 @@
+package test.java.br.ufg.inf.es.relprev.client.dominio.tests;
+
+import junit.framework.TestCase;
+import test.java.br.ufg.inf.es.relprev.client.dominio.Relator;
+import test.java.br.ufg.inf.es.relprev.client.dominio.Relprev;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * User: halisson
+ * Date: 11/2/13
+ * Time: 1:30 AM
+ */
+public class RelprevTests extends TestCase {
+
+    public void testRelprevDeveConterLocal() {
+        Relprev relprev = new Relprev();
+        relprev.setLocal("Local");
+        assertEquals("Local", relprev.getLocal());
+    }
+
+    public void testRelprevDeveConterData() {
+        Relprev relprev = new Relprev();
+        Date data = new Date();
+        relprev.setData(data);
+        assertEquals(data, relprev.getData());
+    }
+
+    public void testRelprevDeveConterEnvolvidos() {
+        Relprev relprev = new Relprev();
+        String pessoalEnvolvido = "Mecanico A e B";
+        relprev.setPessoalEnvolvido(pessoalEnvolvido);
+        assertEquals(pessoalEnvolvido, relprev.getPessoalEnvolvido());
+    }
+
+    public void testRelprevDeveConterSituacao() {
+        Relprev relprev = new Relprev();
+        String situacao = "Situação de risco ao fazer tal e tal coisa =x";
+        relprev.setSituacao(situacao);
+        assertEquals(situacao, relprev.getSituacao());
+    }
+
+    public void testRelprevDeveConterAnexos() {
+        Relprev relprev = new Relprev();
+        List<File> anexos = new ArrayList<File>();
+        relprev.setAnexos(anexos);
+        assertEquals(anexos, relprev.getAnexos());
+    }
+
+    public void testRelprevDeveConterRelator() {
+        Relprev relprev = new Relprev();
+        Relator relator = new Relator();
+        String nome = "RelatorShow";
+        String contato = "RelatorShow@show.com";
+        relator.setNome(nome);
+        relator.setContato(contato);
+        relprev.setRelator(relator);
+        assertEquals(nome, relprev.getNomeRelator());
+        assertEquals(contato, relprev.getContatoRelator());
+    }
+
+    public void testRelprevSeNaoTiverRelatorDeveRetornarVazioParaONomeEContato() {
+        Relprev relprev = new Relprev();
+        String nome = "";
+        String contato = "";
+        assertEquals(nome, relprev.getNomeRelator());
+        assertEquals(contato, relprev.getContatoRelator());
+    }
+
+    //TODO: Corrigir para usar mocks e mockar requisição real
+    public void testDeveSerPossivelListarOsRelprevsExistentes() {
+        assertNotNull(Relprev.list());
+    }
+}
