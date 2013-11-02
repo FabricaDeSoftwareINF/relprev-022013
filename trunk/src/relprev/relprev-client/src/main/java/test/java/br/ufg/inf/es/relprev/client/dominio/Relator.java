920802e212187998992c1fbc9090c8fdb262f584
halissonvit@gmail.com <halissonvit@gmail.com@92d7c9eb-1257-281d-8874-32b03f7a9dd1>
2013-11-02 05:33:36 +0000

    ref #141 Criando versão inicial(mockada) do client, para que possa ser utilizada no front-end. Com ela já é possível utilizar a api para listar todos os Relprevs, instanciar um novo Relprev, salvar o relprev(novo ou editando)

---
+++ trunk/src/relprev/relprev-client/src/main/java/test/java/br/ufg/inf/es/relprev/client/dominio/Relator.java
@@ -0,0 +1,27 @@
+package test.java.br.ufg.inf.es.relprev.client.dominio;
+
+/**
+ * User: halisson
+ * Date: 11/2/13
+ */
+public class Relator {
+
+    private String nome;
+    private String contato;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+}
