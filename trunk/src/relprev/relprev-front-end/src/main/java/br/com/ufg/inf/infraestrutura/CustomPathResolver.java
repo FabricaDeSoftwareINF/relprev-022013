7d2180e165c3f8074a0c1f8321debaa3fd261c81
claudio.cth@gmail.com <claudio.cth@gmail.com@92d7c9eb-1257-281d-8874-32b03f7a9dd1>
2013-11-02 12:11:24 +0000

    ref #124 Controller do relatório, CustomPathResolver que muda o caminho padrão das páginas e log4j.xml para os logs no console.

---
+++ trunk/src/relprev/relprev-front-end/src/main/java/br/com/ufg/inf/infraestrutura/CustomPathResolver.java
@@ -0,0 +1,23 @@
+package br.com.ufg.inf.infraestrutura;
+
+import br.com.caelum.vraptor.http.FormatResolver;
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.view.DefaultPathResolver;
+
+@Component
+public class CustomPathResolver extends DefaultPathResolver {
+
+    public CustomPathResolver(FormatResolver resolver) {
+		super(resolver);
+	}
+
+	@Override
+    protected String getPrefix() {
+        return "/WEB-INF/paginas/";
+    }
+	
+	@Override
+    protected String getExtension() {
+        return "html";
+    }
+}
