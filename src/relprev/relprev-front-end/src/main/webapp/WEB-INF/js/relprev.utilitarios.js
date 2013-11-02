c9326c3bb64df1a0eeb971cd2339ea7e28a9848d
claudio.cth <claudio.cth@gmail.com>
2013-11-02 12:06:44 +0000

    ref #124 Comitando as classes que foram incluidas em outra pasta segundo estrutura do framework que foi alterada do padrão do vraptor; Alterado de WEB-INF/jsp para WEB-INF/paginas.

---
+++ src/relprev/relprev-front-end/src/main/webapp/WEB-INF/js/relprev.utilitarios.js
@@ -0,0 +1,10 @@
+relprev.utilitarios = (function(instancia) {
+	if (instancia) {
+		return instancia;
+	}
+		
+	return {		
+	};
+})(relprev.utilitarios || undefined);
+
+var relprev = window.relprev || {};
