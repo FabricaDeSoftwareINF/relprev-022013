0f309c5c9778200fb39277ca98959a861fb80ba0
claudio.cth@gmail.com <claudio.cth@gmail.com@92d7c9eb-1257-281d-8874-32b03f7a9dd1>
2013-11-07 02:37:11 +0000

    ref #207 Upload de múltiplos arquivos está sendo possível, inclusão do commons upload no pom.xml. Alteração do estilo do input file.

--- trunk/src/relprev/relprev-front-end/src/main/java/br/com/ufg/inf/controller/RelatorioController.java
+++ trunk/src/relprev/relprev-front-end/src/main/java/br/com/ufg/inf/controller/RelatorioController.java
@@ -1,10 +1,13 @@
 package br.com.ufg.inf.controller;
 
+import java.io.File;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
 import br.ufg.inf.es.relprev.client.dominio.Relprev;
 
 @Resource
@@ -18,21 +21,26 @@ public class RelatorioController {
 
 	public void relatorio() {}
 
-	public void salvar(final Relprev relprev) {
+	public void salvar(final Relprev relprev, final List<UploadedFile> files) {
 		relprev.save();
-		this.gerarLog(relprev);
+		this.gerarLog(relprev,files);
 		this.result.redirectTo(RelatorioController.class).relatorio();
 	}
-
-	// Remover este método após conclusão da classe.
-	private void gerarLog(final Relprev relprev) {
+	
+	//Remover este método após conclusão da classe.
+	private void gerarLog(final Relprev relprev, final List<UploadedFile> files){
 		final Logger logger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
 		logger.log(Level.OFF, "Local: " + relprev.getLocal());
 		logger.info("Situação: " + relprev.getSituacao());
 		logger.info("Pessoal envolvido: " + relprev.getPessoalEnvolvido());
-		if (relprev.getRelator() != null) {
+		if(relprev.getRelator() != null){
 			logger.info("Nome do relator: " + relprev.getRelator().getNome());
 			logger.info("Contato do relator: " + relprev.getRelator().getContato());
+		}
+		
+		if(files!= null && files.size() > 0){
+			logger.info(files.size() +"");
+			logger.info(files.get(0)+"");
 		}
 	}
 }
