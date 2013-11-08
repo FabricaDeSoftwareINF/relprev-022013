3dad5436895e0472e639b03455fd3e6b8346649b
danilo.seusaraiva@gmail.com <danilo.seusaraiva@gmail.com@92d7c9eb-1257-281d-8874-32b03f7a9dd1>
2013-11-08 17:21:27 +0000

    ref #219 Utilizada arquitetura criada na revisão 235. - criado repositório para acesso a Relator; - criado serviço para listagens e CRUD Relator;

---
+++ trunk/src/relprev/relprev-webservice/src/main/java/br/ufg/inf/service/RelatorWebService.java
@@ -0,0 +1,30 @@
+package br.ufg.inf.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.ufg.inf.model.Relator;
+import br.ufg.inf.repository.RelatorRepository;
+import br.ufg.inf.service.support.WebServicesURL;
+
+/**
+ * Serviços para os Relatores de Relprev.
+ * 
+ * @created 05/11/2013
+ * @author Danilo Guimarães J. Lemes - <a
+ *         href="mailto:danilo.seusaraiva@gmail.com">danilo.seusaraiva@gmail.com</a>
+ */
+@Controller
+@RequestMapping(value = WebServicesURL.URL_RELATOR)
+public class RelatorWebService extends GenericWebService<Relator, RelatorRepository> {
+
+    @Autowired
+    private RelatorRepository repository;
+    
+    @Override
+    public RelatorRepository getRepository() {
+        return this.repository;
+    }
+
+}
