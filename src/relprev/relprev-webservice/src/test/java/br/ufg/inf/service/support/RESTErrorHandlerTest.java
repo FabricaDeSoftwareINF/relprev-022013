0492f3d287d2d0068b5ab5c282ec87d2f2e60f24
rmbarca <rmbarca@gmail.com>
2013-11-29 23:05:06 +0000

    ref #287 Teste unitário

---
+++ src/relprev/relprev-webservice/src/test/java/br/ufg/inf/service/support/RESTErrorHandlerTest.java
@@ -0,0 +1,45 @@
+package br.ufg.inf.service.support;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+
+import br.ufg.inf.model.support.AbstractEntity;
+/**
+ * Testes para RESTErrorHandler
+ * @author Raul
+ *
+ */
+public class RESTErrorHandlerTest<E extends AbstractEntity<E>> {
+	@Mock
+	private HttpClientErrorException clientException;
+	
+	@Mock
+	private HttpServerErrorException serverException;
+	
+	private RESTErrorHandler<E> restErrorHandler;
+	
+	@Before
+	public void setUp(){
+		MockitoAnnotations.initMocks(this);
+		restErrorHandler = new RESTErrorHandler<E>();
+	}
+
+	@Test
+	public void testProcessHttpClientErrorException() {
+		Response<E> r = restErrorHandler.processHttpClientErrorException(clientException);
+		assertNotNull(r);
+	}
+	
+	@Test
+	public void testProcessHttpServerErrorException() {
+		Response<E> r = restErrorHandler.processHttpServerErrorException(serverException);
+		assertNotNull(r);
+	}
+	
+}
