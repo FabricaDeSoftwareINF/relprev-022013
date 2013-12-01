dfe1169a402393239005faf79e747e6df3946f53
daynilou@gmail.com <daynilou@gmail.com@92d7c9eb-1257-281d-8874-32b03f7a9dd1>
2013-12-01 19:10:20 +0000

    ref #261 - Ajuste na página inicial. - Mock dos relatórios. - Utilização dos dados mockados na view.

--- trunk/src/relprev/relprev-front-end/src/main/java/br/ufg/inf/es/relprev/controller/ImpressaoController.java
+++ trunk/src/relprev/relprev-front-end/src/main/java/br/ufg/inf/es/relprev/controller/ImpressaoController.java
@@ -1,8 +1,16 @@
 package br.ufg.inf.es.relprev.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.ufg.inf.es.relprev.client.dominio.EloSipaer;
+import br.ufg.inf.es.relprev.client.dominio.Relator;
+import br.ufg.inf.es.relprev.client.dominio.Relprev;
+import br.ufg.inf.es.relprev.client.http.exception.RequestException;
 
 @Resource
 public class ImpressaoController {
@@ -14,6 +22,40 @@ public class ImpressaoController {
 	}
 
 	@Path("/impressao")
-	public void inicio() {
+	public List<Relprev> inicio() {
+		/*
+		 * try { relatorios = new Relprev().list(); } catch (RequestException e)
+		 * { e.printStackTrace(); }
+		 * 
+		 * return relatorios;
+		 */
+		List<Relprev> relatorios = new ArrayList<Relprev>();
+		Relprev relatorio = new Relprev();
+		relatorio.id = 1;
+		relatorio.setDataHora(new Date(2011, 11, 27));
+		relatorio.setSituacao("Pegaram meu checklist no bolso do meu anti-g e colocaram outro...");
+		Relator relator = new Relator();
+		relator.setNome("Cap. José");
+		relatorio.setRelator(relator);
+		relatorios.add(relatorio);
+		
+		relatorio = new Relprev();
+		relatorio.id = 2;
+		relatorio.setDataHora(new Date(2012, 01, 01));
+		relatorio.setSituacao("Objeto estranho encontrado no chão dos hangaretes");
+		relator = new Relator();
+		relator.setNome("3s Pagan");
+		relatorio.setRelator(relator);
+		relatorios.add(relatorio);
+		
+		relatorio = new Relprev();
+		relatorio.id = 3;
+		relatorio.setDataHora(new Date(2012, 0, 05));
+		relatorio.setSituacao("Objeto estranho encontrado na caixa de partida da nacele do 1p");
+		relator = new Relator();
+		relator.setNome("3s Gilbert");
+		relatorio.setRelator(relator);
+		relatorios.add(relatorio);
+		return relatorios;		
 	}
 }
