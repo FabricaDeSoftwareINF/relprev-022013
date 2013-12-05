b56f6e11fbe549077bb6a351b5c8149ed9bc9733
bruno <bruno@brunocesar.com>
2013-12-05 22:48:39 +0000

    ref #320 arrumando erro de compilação

--- src/relprev/relprev-webservice/src/test/java/br/ufg/inf/service/RelatorioDePrevencaoWebServiceTest.java
+++ src/relprev/relprev-webservice/src/test/java/br/ufg/inf/service/RelatorioDePrevencaoWebServiceTest.java
@@ -14,17 +14,15 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.ufg.inf.model.AcaoRecomendada;
+import br.ufg.inf.model.ClassificacaoRisco;
 import br.ufg.inf.model.EloSipaer;
 import br.ufg.inf.model.RelatorioPrevencao;
 import br.ufg.inf.model.Situacao;
-import br.ufg.inf.model.ClassificacaoRisco;
 import br.ufg.inf.model.security.Usuario;
 import br.ufg.inf.repository.EloSipaerRepository;
 import br.ufg.inf.repository.RelatorioDePrevencaoRepository;
 import br.ufg.inf.repository.support.LogRepository;
 import br.ufg.inf.service.support.Response;
-
-
 
 /**
  * Testes para os endpoints REST dos Relatórios de Prevenção
@@ -35,258 +33,262 @@ import br.ufg.inf.service.support.Response;
  * @see RelatorioPrevencaoWebService
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:META-INF/spring/applicationContextTest-mvc.xml","classpath:META-INF/spring/applicationContextTest-persistence.xml"})
+@ContextConfiguration({"classpath:META-INF/spring/applicationContextTest-mvc.xml",
+        "classpath:META-INF/spring/applicationContextTest-persistence.xml"})
 @Transactional
 @TransactionConfiguration(defaultRollback = true)
 public class RelatorioDePrevencaoWebServiceTest {
-	
-	@Autowired
-	private RelatorioDePrevencaoRepository relatorioDePrevencaoRepository;
-	
-	@Autowired
-	private EloSipaerRepository eloSipaerRepository;
-    
-	@Autowired
-    private LogRepository logRepository; 
-    
+
+    @Autowired
+    private RelatorioDePrevencaoRepository relatorioDePrevencaoRepository;
+
+    @Autowired
+    private EloSipaerRepository eloSipaerRepository;
+
+    @Autowired
+    private LogRepository logRepository;
+
     private RelatorioDePrevencaoWebService relatorioDePrevencaoWebService;
-    
+
     private RelatorioPrevencao relprev;
 
     @Before
     public void setUp() {
-    	relatorioDePrevencaoWebService = new RelatorioDePrevencaoWebService(relatorioDePrevencaoRepository, logRepository);
-    	
-    	relprev = new RelatorioPrevencao();
-    	
-    	Usuario usuario = new Usuario();
-    	usuario.setAtivo(true);
-    	usuario.setDataInsercaoAlteracao(new Date());
-    	usuario.setNomeCompleto("teste nome");
-    	usuario.setFuncao("funçaõ");
-    	usuario.setPosto("posto");
-    	usuario.setSenha("d131dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f89d15431dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f");
-    	usuario.setSiglaSecao("SIGLA");
-    	usuario.setUsuario("usuario");
-    	
-    	EloSipaer eloSipaer = new EloSipaer();
-    	eloSipaer.setDataInsercaoAlteracao(new Date());
-    	eloSipaer.setOrganizacao("teste organização");
-    	eloSipaer.setSiglaOrganizacao("SIGLA");
-    	eloSipaer.setUsuario(usuario);
-    	
-    	eloSipaerRepository.save(eloSipaer);
-    	
-    	Situacao situacao = new Situacao();
-    	situacao.setFoiConcluido(false);
-    	situacao.setTemAcaoRecomendada(false);
-    	situacao.setTemDivulgacao(false);
-    	situacao.setTemEncaminhamento(false);
-    	
-    	relprev.setDataInsercaoAlteracao(new Date());
-    	relprev.setDataSituacaoPerigosa(new Date());
-    	relprev.setDescricaoSituacaoPerigosa("teste descrição");
-    	relprev.setEloSipaer(eloSipaer);
-    	relprev.setEnvolvidos("teste envolvidos");
-    	relprev.setLocal("teste local");
-    	relprev.setSituacoes(situacao);
-    	
-    	relatorioDePrevencaoWebService.create(relprev);
+        this.relatorioDePrevencaoWebService = new RelatorioDePrevencaoWebService(this.relatorioDePrevencaoRepository,
+                this.logRepository);
+
+        this.relprev = new RelatorioPrevencao();
+
+        final Usuario usuario = new Usuario();
+        usuario.setAtivo(true);
+        usuario.setDataInsercaoAlteracao(new Date());
+        usuario.setNomeCompleto("teste nome");
+        usuario.setFuncao("funçaõ");
+        usuario.setPosto("posto");
+        usuario.setSenha("d131dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f89d15431dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f");
+        usuario.setSiglaSecao("SIGLA");
+        usuario.setUsuario("usuario");
+
+        final EloSipaer eloSipaer = new EloSipaer();
+        eloSipaer.setDataInsercaoAlteracao(new Date());
+        eloSipaer.setOrganizacao("teste organização");
+        eloSipaer.setSiglaOrganizacao("SIGLA");
+        eloSipaer.setUsuario(usuario);
+
+        this.eloSipaerRepository.save(eloSipaer);
+
+        final Situacao situacao = new Situacao();
+        situacao.setFoiConcluido(false);
+        situacao.setTemAcaoRecomendada(false);
+        situacao.setTemDivulgacao(false);
+        situacao.setTemEncaminhamento(false);
+
+        this.relprev.setDataInsercaoAlteracao(new Date());
+        this.relprev.setDataSituacaoPerigosa(new Date());
+        this.relprev.setDescricaoSituacaoPerigosa("teste descrição");
+        this.relprev.setEloSipaer(eloSipaer);
+        this.relprev.setEnvolvidos("teste envolvidos");
+        this.relprev.setLocal("teste local");
+        this.relprev.setSituacoes(situacao);
+
+        this.relatorioDePrevencaoWebService.create(this.relprev);
     }
 
     @Test
     public void testFindRelPrevByLocal() throws Exception {
-    	Response<?> r = relatorioDePrevencaoWebService.findRelPrevByLocal("teste local");
-    	
-    	assertNotNull(r);
+        final Response<?> r = this.relatorioDePrevencaoWebService.findRelPrevByLocal("teste local");
+
+        assertNotNull(r);
     }
 
     @Test
     public void testFindRelPrevByDescricao() throws Exception {
-    	Response<?> r = relatorioDePrevencaoWebService.findRelPrevByDescricao("teste descrição");
-    	
-    	assertNotNull(r);
+        final Response<?> r = this.relatorioDePrevencaoWebService.findRelPrevByDescricao("teste descrição");
+
+        assertNotNull(r);
     }
-    
+
     @Test
     public void testFindRelPrevByLocalException() throws Exception {
-    	relatorioDePrevencaoWebService = new RelatorioDePrevencaoWebService(null, logRepository);
-    	Response<RelatorioPrevencao> r = relatorioDePrevencaoWebService.findRelPrevByLocal("Teste");
-    	
-    	assertNotNull(r);
+        this.relatorioDePrevencaoWebService = new RelatorioDePrevencaoWebService(null, this.logRepository);
+        final Response<RelatorioPrevencao> r = this.relatorioDePrevencaoWebService.findRelPrevByLocal("Teste");
+
+        assertNotNull(r);
     }
 
     @Test
     public void testFindRelPrevByDescricaoException() throws Exception {
-    	relatorioDePrevencaoWebService = new RelatorioDePrevencaoWebService(null, logRepository);
-    	Response<?> r = relatorioDePrevencaoWebService.findRelPrevByDescricao("Teste");
-    	
-    	assertNotNull(r);
+        this.relatorioDePrevencaoWebService = new RelatorioDePrevencaoWebService(null, this.logRepository);
+        final Response<?> r = this.relatorioDePrevencaoWebService.findRelPrevByDescricao("Teste");
+
+        assertNotNull(r);
     }
-    
+
     @Test
     public void testUpdate() throws Exception {
-    	relprev.setEnvolvidos("env2");
+        this.relprev.setEnvolvidos("env2");
 
-    	Response<?> r =relatorioDePrevencaoWebService.update(relprev);
-    	assertNotNull(r.getMessage());
+        final Response<?> r = this.relatorioDePrevencaoWebService.update(this.relprev);
+        assertNotNull(r.getMessage());
     }
-    
+
     @Test
     public void testDelete() throws Exception {
-    	Response<?> r =relatorioDePrevencaoWebService.delete(relprev.getId());
-    	System.out.println(r.getMessage());
-    }
-    
-    @Test
-    public void testAddAcaoRecomendada() throws Exception {
-    	
-    	relatorioDePrevencaoWebService.update(relprev);
-    	System.out.println("\n\n"+relatorioDePrevencaoWebService.list().getData());
-    	
-    	AcaoRecomendada acaoRecomendada = new AcaoRecomendada();
-    	acaoRecomendada.setDescricao("ação descrição");
-    	acaoRecomendada.setDestinatario("destinatario");
-    	acaoRecomendada.setRemetente("remetente");
-    	acaoRecomendada.setRelPrev(relprev);
-    	
-    	Response<?> r = relatorioDePrevencaoWebService.addAcaoRecomendada(relprev.getId(), acaoRecomendada);
-    	assertNotNull(r);
-    }
-    
-    @Test
-    public void testUpdateAcaoRecomendada() throws Exception {
-    	
-    	relatorioDePrevencaoWebService.update(relprev);
-    	System.out.println("\n\n"+relatorioDePrevencaoWebService.list().getData());
-    	
-    	AcaoRecomendada acaoRecomendada = new AcaoRecomendada();
-    	acaoRecomendada.setDescricao("ação descrição");
-    	acaoRecomendada.setDestinatario("destinatario 2");
-    	acaoRecomendada.setRemetente("remetente 2");
-    	acaoRecomendada.setRelPrev(relprev);
-    	
-    	Response<?> r = relatorioDePrevencaoWebService.updateAcaoRecomendada(relprev.getId(), acaoRecomendada);
-    	assertNotNull(r);
-    }
-    
-    @Test
-    public void testAddClassificacaoRisco() throws Exception {
-    	
-    	//relatorioDePrevencaoWebService.update(relprev);
-    	//System.out.println("\n\n"+relatorioDePrevencaoWebService.list().getData());
-    	
-    	ClassificacaoRisco classificacaoRisco = new ClassificacaoRisco();
-    	classificacaoRisco.setAvaliacaoInicial("AA");
-    	classificacaoRisco.setAvaliacaoFinal("AB");
-    	classificacaoRisco.setRelPrev(relprev);
-    	
-    	Response<?> r = relatorioDePrevencaoWebService.addClassificacaoRisco(relprev.getId(), classificacaoRisco);
-    	assertNotNull(r);
-    }
-    
-    @Test
-    public void testUpdateClassificacaoRisco() throws Exception {
-    	
-    	//relatorioDePrevencaoWebService.update(relprev);
-    	//System.out.println("\n\n"+relatorioDePrevencaoWebService.list().getData());
-    	
-    	ClassificacaoRisco classificacaoRisco = new ClassificacaoRisco();
-    	classificacaoRisco.setAvaliacaoInicial("AA");
-    	classificacaoRisco.setAvaliacaoFinal("AB");
-    	classificacaoRisco.setRelPrev(relprev);
-    	
-    	Response<?> r = relatorioDePrevencaoWebService.updateClassificacaoRisco(relprev.getId(), classificacaoRisco);
-    	assertNotNull(r);
-    }
-        
-    @Test(expected = NullPointerException.class) 
-    public void testAddAcaoRecomendadaException() throws Exception {
-    	relatorioDePrevencaoWebService.addAcaoRecomendada(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testUpdateAcaoRecomendadaException() throws Exception {
-    	relatorioDePrevencaoWebService.updateAcaoRecomendada(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testDeleteAcaoRecomendadaException() throws Exception {
-    	relatorioDePrevencaoWebService.deleteAcaoRecomendada(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testAddClassificacaoDeRiscoException() throws Exception {
-    	relatorioDePrevencaoWebService.addClassificacaoDeRisco(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testUpdateClassificacaoDeRiscoException() throws Exception {
-    	relatorioDePrevencaoWebService.updateClassificacaoDeRisco(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testDeleteClassificacaoDeRiscoException() throws Exception {
-    	relatorioDePrevencaoWebService.deleteClassificacaoDeRisco(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testAddEncaminhamentoException() throws Exception {
-    	relatorioDePrevencaoWebService.addEncaminhamento(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testUpdateEncaminhamentoException() throws Exception {
-    	relatorioDePrevencaoWebService.updateEncaminhamento(1L, null);
-    }
-    
-    @Test(expected = NullPointerException.class) 
-    public void testDeleteEncaminhamentoException() throws Exception {
-    	relatorioDePrevencaoWebService.deleteEncaminhamento(1L, null);
+        final Response<?> r = this.relatorioDePrevencaoWebService.delete(this.relprev.getId());
+        System.out.println(r.getMessage());
     }
 
-    @Test(expected = NullPointerException.class) 
+    @Test
+    public void testAddAcaoRecomendada() throws Exception {
+
+        this.relatorioDePrevencaoWebService.update(this.relprev);
+        System.out.println("\n\n" + this.relatorioDePrevencaoWebService.list().getData());
+
+        final AcaoRecomendada acaoRecomendada = new AcaoRecomendada();
+        acaoRecomendada.setDescricao("ação descrição");
+        acaoRecomendada.setDestinatario("destinatario");
+        acaoRecomendada.setRemetente("remetente");
+        acaoRecomendada.setRelPrev(this.relprev);
+
+        final Response<?> r = this.relatorioDePrevencaoWebService.addAcaoRecomendada(this.relprev.getId(), acaoRecomendada);
+        assertNotNull(r);
+    }
+
+    @Test
+    public void testUpdateAcaoRecomendada() throws Exception {
+
+        this.relatorioDePrevencaoWebService.update(this.relprev);
+        System.out.println("\n\n" + this.relatorioDePrevencaoWebService.list().getData());
+
+        final AcaoRecomendada acaoRecomendada = new AcaoRecomendada();
+        acaoRecomendada.setDescricao("ação descrição");
+        acaoRecomendada.setDestinatario("destinatario 2");
+        acaoRecomendada.setRemetente("remetente 2");
+        acaoRecomendada.setRelPrev(this.relprev);
+
+        final Response<?> r = this.relatorioDePrevencaoWebService.updateAcaoRecomendada(this.relprev.getId(), acaoRecomendada);
+        assertNotNull(r);
+    }
+
+    @Test
+    public void testAddClassificacaoRisco() throws Exception {
+
+        // relatorioDePrevencaoWebService.update(relprev);
+        // System.out.println("\n\n"+relatorioDePrevencaoWebService.list().getData());
+
+        final ClassificacaoRisco classificacaoRisco = new ClassificacaoRisco();
+        classificacaoRisco.setAvaliacaoInicial("AA");
+        classificacaoRisco.setAvaliacaoFinal("AB");
+        classificacaoRisco.setRelPrev(this.relprev);
+
+        final Response<?> r = this.relatorioDePrevencaoWebService.addClassificacaoDeRisco(this.relprev.getId(),
+                classificacaoRisco);
+        assertNotNull(r);
+    }
+
+    @Test
+    public void testUpdateClassificacaoRisco() throws Exception {
+
+        // relatorioDePrevencaoWebService.update(relprev);
+        // System.out.println("\n\n"+relatorioDePrevencaoWebService.list().getData());
+
+        final ClassificacaoRisco classificacaoRisco = new ClassificacaoRisco();
+        classificacaoRisco.setAvaliacaoInicial("AA");
+        classificacaoRisco.setAvaliacaoFinal("AB");
+        classificacaoRisco.setRelPrev(this.relprev);
+
+        final Response<?> r = this.relatorioDePrevencaoWebService.updateClassificacaoDeRisco(this.relprev.getId(),
+                classificacaoRisco);
+        assertNotNull(r);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddAcaoRecomendadaException() throws Exception {
+        this.relatorioDePrevencaoWebService.addAcaoRecomendada(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testUpdateAcaoRecomendadaException() throws Exception {
+        this.relatorioDePrevencaoWebService.updateAcaoRecomendada(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDeleteAcaoRecomendadaException() throws Exception {
+        this.relatorioDePrevencaoWebService.deleteAcaoRecomendada(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddClassificacaoDeRiscoException() throws Exception {
+        this.relatorioDePrevencaoWebService.addClassificacaoDeRisco(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testUpdateClassificacaoDeRiscoException() throws Exception {
+        this.relatorioDePrevencaoWebService.updateClassificacaoDeRisco(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDeleteClassificacaoDeRiscoException() throws Exception {
+        this.relatorioDePrevencaoWebService.deleteClassificacaoDeRisco(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddEncaminhamentoException() throws Exception {
+        this.relatorioDePrevencaoWebService.addEncaminhamento(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testUpdateEncaminhamentoException() throws Exception {
+        this.relatorioDePrevencaoWebService.updateEncaminhamento(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDeleteEncaminhamentoException() throws Exception {
+        this.relatorioDePrevencaoWebService.deleteEncaminhamento(1L, null);
+    }
+
+    @Test(expected = NullPointerException.class)
     public void testAddObservacaoException() throws Exception {
-    	relatorioDePrevencaoWebService.addObservacao(1L, null);
+        this.relatorioDePrevencaoWebService.addObservacao(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testUpdateObservacaoException() throws Exception {
-    	relatorioDePrevencaoWebService.updateObservacao(1L, null);
+        this.relatorioDePrevencaoWebService.updateObservacao(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testDeleteObservacaoException() throws Exception {
-    	relatorioDePrevencaoWebService.deleteObservacao(1L, null);
+        this.relatorioDePrevencaoWebService.deleteObservacao(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testAddParecerSetorException() throws Exception {
-    	relatorioDePrevencaoWebService.addParecerSetor(1L, null);
+        this.relatorioDePrevencaoWebService.addParecerSetor(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testUpdateParecerSetorException() throws Exception {
-    	relatorioDePrevencaoWebService.updateParecerSetor(1L, null);
+        this.relatorioDePrevencaoWebService.updateParecerSetor(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testDeleteParecerSetorException() throws Exception {
-    	relatorioDePrevencaoWebService.deleteParecerSetor(1L, null);
+        this.relatorioDePrevencaoWebService.deleteParecerSetor(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testAddRespostaException() throws Exception {
-    	relatorioDePrevencaoWebService.addResposta(1L, null);
+        this.relatorioDePrevencaoWebService.addResposta(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testUpdateRespostaException() throws Exception {
-    	relatorioDePrevencaoWebService.updateResposta(1L, null);
+        this.relatorioDePrevencaoWebService.updateResposta(1L, null);
     }
-    
-    @Test(expected = NullPointerException.class) 
+
+    @Test(expected = NullPointerException.class)
     public void testDeleteRespostaException() throws Exception {
-    	relatorioDePrevencaoWebService.deleteResposta(1L, null);
+        this.relatorioDePrevencaoWebService.deleteResposta(1L, null);
     }
 }
