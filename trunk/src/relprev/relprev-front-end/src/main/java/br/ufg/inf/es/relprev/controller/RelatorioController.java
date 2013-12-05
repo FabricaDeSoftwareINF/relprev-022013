17f9297e72e22c2fc41aaa3102b9343f3a8a3601
halissonvit@gmail.com <halissonvit@gmail.com@92d7c9eb-1257-281d-8874-32b03f7a9dd1>
2013-12-05 02:38:26 +0000

    ref #324 [WIP] comitando uma versão inicial, e parcial, com algumas coisas ainda mockadas, da implementação da persistencia dos arquivos para fazer CDN

--- trunk/src/relprev/relprev-front-end/src/main/java/br/ufg/inf/es/relprev/controller/RelatorioController.java
+++ trunk/src/relprev/relprev-front-end/src/main/java/br/ufg/inf/es/relprev/controller/RelatorioController.java
@@ -1,8 +1,12 @@
 package br.ufg.inf.es.relprev.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,7 +14,10 @@ import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.interceptor.multipart.DefaultUploadedFile;
 import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
+import br.ufg.inf.es.relprev.cdn.CDN;
+import br.ufg.inf.es.relprev.client.dominio.Anexo;
 import br.ufg.inf.es.relprev.client.dominio.RelatorioPrevencao;
 import br.ufg.inf.es.relprev.client.http.exception.RequestException;
 import br.ufg.inf.es.relprev.infraestrutura.ResultadoServico;
@@ -18,65 +25,86 @@ import br.ufg.inf.es.relprev.infraestrutura.ResultadoServico;
 @Resource
 public class RelatorioController extends GenericController<RelatorioPrevencao> {
 
-	private final Logger logger;
-	private final Result result;
+    private final Logger logger;
+    private final Result result;
 
-	public RelatorioController(final Result result, final ResultadoServico resultado) {
-		super(result, resultado);
-		this.result = result;
-		this.logger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
-	}
-	
-	@Get("/relatorio/inicio") 
-	public void relatorioCompleto() {		
-	}
-	
-	@Path("/relatorio/relatorio")
-	public void relatorio() {}
+    public RelatorioController(final Result result, final ResultadoServico resultado) {
+        super(result, resultado);
+        this.result = result;
+        this.logger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+    }
 
-	@Path("/relatorio/dadosgerais/{id}")
-	public RelatorioPrevencao dadosGerais(Integer id) {
-		try {
-			return (RelatorioPrevencao) new RelatorioPrevencao().get(id);
-		} catch (RequestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new RelatorioPrevencao();
-	}
-	
-	public List<RelatorioPrevencao> ultimosRelatorios(){
-		List<RelatorioPrevencao> lista = new ArrayList<RelatorioPrevencao>();
-		try {
-			lista = new RelatorioPrevencao().list();
-		} catch (RequestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return lista;
-	}
-	
-	//Remover este método após conclusão da classe.
-	protected void gerarLog(final RelatorioPrevencao relprev){
-		logger.log(Level.OFF, "Local: " + relprev.getLocal());
-		logger.info("Situação: " + relprev.getSituacao());
-		logger.info("Pessoal envolvido: " + relprev.getEnvolvidos());
-		
-		if(relprev.getRelator() != null){
-			logger.info("Nome do relator: " + relprev.getRelator().getNome());
-			logger.info("Telefone do relator: " + relprev.getRelator().getTelefoneCelular());
-			logger.info("Email do relator: " + relprev.getRelator().getEmail());			
-		}
-		
+    public void saveWithFiles(RelatorioPrevencao relatorioPrevencao, List<UploadedFile> files) throws FileNotFoundException {
+        if (files == null) {
+            files = new ArrayList<UploadedFile>();
+
+            File file = new File("/media/halisson/dados/dev/faculdade/relprev-022013/src/relprev/relprev-front-end/src/pom.xml");
+            DefaultUploadedFile uploadedFile = new DefaultUploadedFile(new FileInputStream(file), "pom.xml", "TXT", file.length());
+
+            files.add(uploadedFile);
+        }
+        relatorioPrevencao.setAnexos(CDN.save(files));
+        super.save(relatorioPrevencao);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Get("/relatorio/inicio")
+    public void relatorioCompleto() {
+    }
+
+    @Path("/relatorio/relatorio")
+    public void relatorio() {
+    }
+
+    @Path("/relatorio/dadosgerais/{id}")
+    public RelatorioPrevencao dadosGerais(Integer id) {
+        try {
+            return (RelatorioPrevencao) new RelatorioPrevencao().get(id);
+        } catch (RequestException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new RelatorioPrevencao();
+    }
+
+    public List<RelatorioPrevencao> ultimosRelatorios() {
+        List<RelatorioPrevencao> lista = new ArrayList<RelatorioPrevencao>();
+        try {
+            lista = new RelatorioPrevencao().list();
+        } catch (RequestException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    //Remover este método após conclusão da classe.
+    protected void gerarLog(final RelatorioPrevencao relprev) {
+        logger.log(Level.OFF, "Local: " + relprev.getLocal());
+        logger.info("Situação: " + relprev.getSituacao());
+        logger.info("Pessoal envolvido: " + relprev.getEnvolvidos());
+
+        if (relprev.getRelator() != null) {
+            logger.info("Nome do relator: " + relprev.getRelator().getNome());
+            logger.info("Telefone do relator: " + relprev.getRelator().getTelefoneCelular());
+            logger.info("Email do relator: " + relprev.getRelator().getEmail());
+        }
+
+        if (relprev.getAnexos() != null) {
+            for (Anexo anexo : relprev.getAnexos()) {
+                logger.info("Nome do anexo: " + anexo.getPathAnexo());
+                logger.info("Mimetype do anexo: " + anexo.getMimeType());
+            }
+        }
+
 		/*
-		if(files!= null && files.size() > 0){
+        if(files!= null && files.size() > 0){
 			logger.info(files.size() +"");
 			logger.info(files.get(0)+"");
 		}*/
-	}
+    }
 
-	@Override
-	protected Class<RelatorioPrevencao> obtenhaTipo() {
-		return RelatorioPrevencao.class;
-	}
+    @Override
+    protected Class<RelatorioPrevencao> obtenhaTipo() {
+        return RelatorioPrevencao.class;
+    }
 }
