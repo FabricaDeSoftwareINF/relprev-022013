f242d576619dd70af33da1e28f9b8920bc35441c
gustavo.mtborges <gustavo.mtborges@gmail.com>
2013-12-12 16:50:42 +0000

    ref #117 Atualização de documentos GCO

--- src/relprev/relprev-webservice/src/main/java/br/ufg/inf/repository/security/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Repositórios para acesso aos dados das entidades para a segurança do portal Web
- * 
- * @created 19/11/2013
- * @author Bruno César Ribeiro e Silva - <a href="mailto:bruno@brunocesar.com">bruno@brunocesar.com</a>
- */
-package br.ufg.inf.repository.security;
--- src/relprev/relprev-webservice/src/main/java/br/ufg/inf/service/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Endpoints REST
- *
- * @created 02/11/2013
- * @author Bruno César Ribeiro e Silva - <a href="mailto:bruno@brunocesar.com">bruno@brunocesar.com</a>
- */
-package br.ufg.inf.service;
